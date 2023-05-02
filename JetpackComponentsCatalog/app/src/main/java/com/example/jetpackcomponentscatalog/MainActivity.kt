520c5f0e4031f66dea1859ffa977f8376dbcbeea
XeviDev <jv.martinez.mellado@gmail.com>
2023-05-02 21:29:13 +0200

    Button with states

--- JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/MainActivity.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -58,13 +59,17 @@ fun DefaultPreview() {
 
 @Composable
 fun MyButtonExample() {
+    var enabled by rememberSaveable {
+        mutableStateOf(true)
+    }
     Column(
         Modifier
             .fillMaxSize()
             .padding(24.dp)
     ) {
         Button(
-            onClick = { Log.i("Xevi", "Esto es un ejemplo") },
+            onClick = { enabled = false },
+            enabled = enabled,
             colors = ButtonDefaults.buttonColors(
                 backgroundColor = Color.Magenta,
                 contentColor = Color.Blue
