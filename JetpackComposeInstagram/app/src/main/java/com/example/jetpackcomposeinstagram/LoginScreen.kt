5abbed73254a2ea01afed714ea14fa1b7924c6ac
XeviDev <jv.martinez.mellado@gmail.com>
2023-05-23 22:42:42 +0200

    footer finished

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/LoginScreen.kt
@@ -22,6 +22,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
+fun Footer(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        SignUp()
+        Spacer(modifier = Modifier.size(24.dp))
+
+    }
+}
+
+@Composable
+fun SignUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = "Don't have an account?", fontSize = 12.sp, color = Color(0xFFB5B5B5))
+        Text(
+            text = "Sign up.",
+            Modifier.padding(horizontal = 8.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
+    }
+}
+
+@Composable
 fun LoginScreen() {
     Box(
         Modifier
@@ -33,6 +63,7 @@ fun LoginScreen() {
         //Por tanto, se le pasa por parámetros.
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
+        Footer((Modifier.align(Alignment.BottomCenter)))
     }
 }
 
