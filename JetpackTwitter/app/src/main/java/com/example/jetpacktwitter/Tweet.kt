6f984d609a772af7265226e39ec8baab763e902b
XeviDev <jv.martinez.mellado@gmail.com>
2023-05-29 22:36:57 +0200

    wip

--- JetpackTwitter/app/src/main/java/com/example/jetpacktwitter/Tweet.kt
+++ JetpackTwitter/app/src/main/java/com/example/jetpacktwitter/Tweet.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,20 +26,31 @@ fun Tweet() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        val (iconProfile, tweet) = createRefs()
+        val (iconProfile, tweet, divider) = createRefs()
         IconProfile(
             Modifier
                 .constrainAs(iconProfile) {
                     top.linkTo(parent.top)
                     start.linkTo(parent.start)
                 }
-                .background(color = Color.Red))
-        TweetPart(modifier = Modifier.constrainAs(tweet) {
-            top.linkTo(parent.top)
-            start.linkTo(iconProfile.end)
-            end.linkTo(parent.end)
-            width = Dimension.fillToConstraints
-        })
+        )
+        TweetPart(modifier = Modifier
+            .constrainAs(tweet) {
+                top.linkTo(parent.top)
+                start.linkTo(iconProfile.end)
+                end.linkTo(parent.end)
+                width = Dimension.fillToConstraints
+            }
+            .background(color = Color.Green))
+        Divider(
+            Modifier
+                .fillMaxWidth()
+                .background(color = Color.Red)
+                .constrainAs(divider) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(tweet.bottom)
+                })
     }
 
 
@@ -47,7 +59,7 @@ fun Tweet() {
 @Composable
 fun TweetPart(modifier: Modifier) {
     ConstraintLayout(modifier = modifier) {
-        val (header, body) = createRefs()
+        val (header, body, footer) = createRefs()
         HeaderTweet(Modifier.constrainAs(header) {
             top.linkTo(parent.top)
             start.linkTo(parent.start)
@@ -57,7 +69,15 @@ fun TweetPart(modifier: Modifier) {
         BodyTweet(Modifier.constrainAs(body) {
             top.linkTo(header.bottom)
             start.linkTo(parent.start)
+            bottom.linkTo(footer.top)
             end.linkTo(parent.end)
+            width = Dimension.fillToConstraints
+        })
+        FooterTweet(Modifier.constrainAs(footer) {
+            top.linkTo(body.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            bottom.linkTo(parent.bottom)
             width = Dimension.fillToConstraints
         })
 
@@ -65,26 +85,71 @@ fun TweetPart(modifier: Modifier) {
 }
 
 @Composable
+fun FooterTweet(modifier: Modifier) {
+    ConstraintLayout(modifier = modifier) {
+        val (comment, rt, like) = createRefs()
+        Icon(
+            painter = painterResource(id = R.drawable.ic_chat),
+            contentDescription = "optionsTweet",
+            tint = Color.White,
+            modifier = Modifier.constrainAs(comment) {
+//                start.linkTo(parent.start)
+//                top.linkTo(parent.top)
+            }
+
+        )
+        /*Icon(
+            painter = painterResource(id = R.drawable.ic_rt),
+            contentDescription = "optionsTweet",
+            tint = Color.White,
+            modifier = Modifier.constrainAs(rt) {
+                end.linkTo(like.start)
+                top.linkTo(parent.top)
+                start.linkTo(comment.end)
+            }
+
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_like),
+            contentDescription = "optionsTweet",
+            tint = Color.White,
+            modifier = Modifier.constrainAs(like) {
+                end.linkTo(parent.end)
+                top.linkTo(parent.top)
+            }
+
+        )*/
+    }
+}
+
+@Composable
 fun BodyTweet(modifier: Modifier) {
     ConstraintLayout(modifier = modifier) {
         val (text, img) = createRefs()
-        Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius nunc sed ipsum sagittis lobortis. Etiam maximus turpis ac nisl rhoncus, id euismod lorem euismod. Sed commodo risus nisi, ut cursus urna fringilla eget. Vestibulum feugiat metus non semper tincidunt. Proin facilisis rhoncus lacus ac consequat. Curabitur iaculis ligula odio, ac ultrices ex pulvinar vel. Nulla viverra neque vel porttitor dignissim. Aliquam dignissim nunc sapien, a ultrices ex gravida non. Vivamus ac tempus felis, non fringilla leo. Fusce pellentesque augue id magna vehicula ullamcorper a nec nibh. Donec eget sagittis elit. Nam quis metus non risus vestibulum laoreet ut ut sem. Mauris sollicitudin eros ac", modifier = Modifier.constrainAs(text) {
-            top.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-            width = Dimension.fillToConstraints
-        }, color = Color.White,
-        maxLines = 5)
+        Text(
+            text = "Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, non risus vestibulum laoreet ut ut sem. Mauris sollicitudin eros ac",
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+
+            },
+            color = Color.White,
+            maxLines = 5
+        )
         Image(
             painter = painterResource(id = R.drawable.profile),
             contentDescription = "img content",
             contentScale = ContentScale.Crop,
-            modifier = Modifier.clip(RoundedCornerShape(24.dp)).height(200.dp).constrainAs(img) {
-                top.linkTo(text.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                width = Dimension.fillToConstraints
-            })
+            modifier = Modifier
+                .clip(RoundedCornerShape(24.dp))
+                .height(200.dp)
+                .constrainAs(img) {
+                    top.linkTo(text.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+
+                })
     }
 }
 
