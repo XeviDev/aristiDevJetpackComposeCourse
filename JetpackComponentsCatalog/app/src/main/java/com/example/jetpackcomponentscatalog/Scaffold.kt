6bf4fc0f9874e2988bf8d28809adf9860627bbba
XeviDev <jv.martinez.mellado@gmail.com>
2023-06-27 15:59:50 +0200

    First Scaffold

--- JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/Scaffold.kt
@@ -7,33 +7,49 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Dangerous
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ScaffoldExample() {
-    Scaffold(topBar = { MyTopAppBar() }) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            MyTopAppBar() {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        "Has pulsado $it"
+                    )
+                }
+            }
+        },
+        scaffoldState = scaffoldState
+    ) {
 
     }
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text(text = "Mi primera toolbar") },
         backgroundColor = Color.Red,
         contentColor = Color.White,
         elevation = 4.dp, navigationIcon = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Atrás") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
         },
         actions = {
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "buscar")
             }
-            IconButton(onClick = { }) {
+            IconButton(onClick = { onClickIcon("Peligro") }) {
                 Icon(imageVector = Icons.Filled.Dangerous, contentDescription = "Dangerous")
             }
         }
