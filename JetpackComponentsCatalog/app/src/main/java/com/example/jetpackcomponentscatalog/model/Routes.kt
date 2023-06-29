653b1df9e90f3382f43ece2562d9cb73c0d58a5c
XeviDev <jv.martinez.mellado@gmail.com>
2023-06-29 17:38:09 +0200

    adding comment

--- JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/model/Routes.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/example/jetpackcomponentscatalog/model/Routes.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomponentscatalog.model
 
 sealed class Routes(val route:String) {
+    //Objetos en la sealed class empiezan en mayúsculas
     object Pantalla1:Routes("pantalla1")
     object Pantalla2:Routes("pantalla2")
     object Pantalla3:Routes("pantalla3")
