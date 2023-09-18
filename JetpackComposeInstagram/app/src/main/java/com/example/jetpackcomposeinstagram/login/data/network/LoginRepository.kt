171a4f58db4259c130abdef970559d7faf6a8941
XeviDev <jv.martinez.mellado@gmail.com>
2023-09-18 19:09:22 +0200

    Added DI

--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/data/network/LoginRepository.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/data/network/LoginRepository.kt
@@ -1,7 +1,9 @@
 package com.example.jetpackcomposeinstagram.login.data.network
+
+import javax.inject.Inject
+
 //Se encarga de gestionar si la llamada es a una bd, bd interna, un archivo, etc
-class LoginRepository {
-    private val api = LoginService()
+class LoginRepository @Inject constructor(private val api:LoginService){
 
     suspend fun doLogin(user: String, password: String):Boolean {
         return api.doLogin(user, password)
--- JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/domain/LoginUseCase.kt
+++ JetpackComposeInstagram/app/src/main/java/com/example/jetpackcomposeinstagram/login/domain/LoginUseCase.kt
@@ -1,12 +1,12 @@
 package com.example.jetpackcomposeinstagram.login.domain
 
 import com.example.jetpackcomposeinstagram.login.data.network.LoginRepository
+import javax.inject.Inject
 
 //Aqui se encarga de la logica de negocio, por cada accion que querramos hacer se
 // debe de crear un caso de uso y no ha de variar con el tiempo.
 // un caso de uso puede llamar a otro caso de uso
-class LoginUseCase {
-    private val repository = LoginRepository()
+class LoginUseCase @Inject constructor(private val repository: LoginRepository) {
 
     //Al poner operator e invoke nos ahorramos el tener que crear un
     //metodo para hacer la llamada. poniendo loginUseCase accederiamos al invoke
